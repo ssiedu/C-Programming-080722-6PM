74eb0959f9af7cc6661ca695150658e09c34cd8c
SSI Digital <42675358+ssiedu@users.noreply.github.com>
2022-07-18 15:41:50 +0530

    first

---
+++ first.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int x=10;
+	printf("%d",_1x);//10
+	x=20;
+	printf("\n value of x : %d",x);//20
+	getch();
+}
---
+++ welcome.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main()
+{
+	printf("\nWelcome to ssi digital\n");
+	printf("c/c++ class");
+	getch();
+	
+}
